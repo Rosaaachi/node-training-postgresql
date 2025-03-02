4c58e9f60e306746632e8359e2d8b28da74b9e74
Rosa <ss91810@gmail.com>
2025-03-02 22:34:22 +0900

    hw

--- week4/server.js
+++ week4/server.js
@@ -1,21 +1,241 @@
 require("dotenv").config()
 const http = require("http")
 const AppDataSource = require("./db")
+const errHandle = require("./errorHandle")
+
+const isValidString = (value) => {
+  return typeof value === 'string' && value.trim() !== '';
+}
+
+const isNumber = (value) => {
+  return typeof value === 'number' && !isNaN(value);
+}
+
 
 const requestListener = async (req, res) => {
-  const headers = {}
+  const headers = {
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Requested-With",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "PATCH, POST, GET,OPTIONS,DELETE",
+    "Content-Type": "application/json"
+  }
+
   let body = ""
   req.on("data", (chunk) => {
     body += chunk
   })
 
   if (req.url === "/api/credit-package" && req.method === "GET") {
-    
+    try {
+      const data = await AppDataSource.getRepository('CreditPackage').find({
+        select: ['id', 'name', 'credit_amount', 'price']
+      })
+      res.writeHead(200, headers)
+      res.write(JSON.stringify({
+        status: "success",
+        data: data,
+      }))
+      res.end()
+
+    } catch (error) {
+      errHandle(res);
+
+    }
+
   } else if (req.url === "/api/credit-package" && req.method === "POST") {
-    
+    req.on('end', async () => {
+      try {
+        const { name, credit_amount, price } = JSON.parse(body);
+        if (!isValidString(name) || !isNumber(credit_amount) || !isNumber(price)) {
+          res.writeHead(400, headers)
+          res.write(JSON.stringify({
+            status: "failed",
+            message: "欄位未填寫正確",
+          }))
+          res.end()
+          return;
+
+        }
+
+        const creditPackage = AppDataSource.getRepository('CreditPackage')
+        const findreditPackage = await creditPackage.find({
+          where: {
+            name: name
+          }
+        })
+        if (findreditPackage.length > 0) {
+          res.writeHead(409, headers)
+          res.write(JSON.stringify({
+            status: "failed",
+            message: "資料重複",
+          }))
+          res.end()
+          return;
+        }
+
+        const newCreditPackage = creditPackage.create({
+          name,
+          credit_amount,
+          price
+        })
+
+        const result = await creditPackage.save(newCreditPackage)
+
+        res.writeHead(200, headers)
+        res.write(JSON.stringify({
+          status: "success",
+          data: result,
+        }))
+        res.end()
+
+      } catch (error) {
+        errHandle(res);
+
+      }
+    })
+
   } else if (req.url.startsWith("/api/credit-package/") && req.method === "DELETE") {
-    
-  } else if (req.method === "OPTIONS") {
+    try {
+      const creditPackageId = req.url.split('/').pop();
+      if (!isValidString(creditPackageId)) {
+        res.writeHead(400, headers)
+        res.write(JSON.stringify({
+          status: "failed",
+          message: "ID錯誤"
+        }))
+        res.end()
+        return
+      }
+      const result = await AppDataSource.getRepository('CreditPackage').delete(creditPackageId)
+      if (result.affected === 0) {
+        res.writeHead(400, headers)
+        res.write(JSON.stringify({
+          status: "failed",
+          message: "ID錯誤",
+        }))
+        res.end()
+        return;
+      }
+
+      res.writeHead(200, headers)
+      res.write(JSON.stringify({
+        status: "success",
+      }))
+      res.end()
+
+    } catch (error) {
+      errHandle(res);
+
+    }
+
+
+  } else if (req.url === "/api/coaches/skill" && req.method === "GET") {
+    try {
+      const data = await AppDataSource.getRepository('Skill').find({
+        select: ['id', 'name']
+      })
+
+      res.writeHead(200, headers)
+      res.write(JSON.stringify({
+        status: "success",
+        data: data
+
+      }))
+      res.end()
+
+    } catch (error) {
+      errHandle(res);
+
+    }
+
+  } else if (req.url === "/api/coaches/skill" && req.method === "POST") {
+    req.on('end', async () => {
+      try {
+        const { name } = JSON.parse(body);
+        if (!isValidString(name)) {
+          res.writeHead(400, headers)
+          res.write(JSON.stringify({
+            status: "failed",
+            message: "欄位未填寫正確",
+          }))
+          res.end()
+          return;
+        }
+        const skillRepo = AppDataSource.getRepository('Skill')
+        const findSkill = await skillRepo.find({
+          where: {
+            name: name
+          }
+        })
+
+        if (findSkill.length > 0) {
+          res.writeHead(409, headers)
+          res.write(JSON.stringify({
+            status: "failed",
+            message: "資料重複"
+          }))
+          res.end()
+          return;
+        }
+
+        const newSkill = skillRepo.create({ name });
+        const result = await skillRepo.save(newSkill);
+
+        res.writeHead(200, headers)
+        res.write(JSON.stringify({
+          status: "success",
+          data: result
+        }))
+        res.end()
+
+      } catch (error) {
+        errHandle(res);
+
+      }
+    })
+
+
+  } else if (req.url.startsWith("/api/coaches/skill/") && req.method === "DELETE") {
+    try {
+      const skillId = req.url.split('/').pop();
+      if(!isValidString(skillId)){
+        res.writeHead(400, headers)
+      res.write(JSON.stringify({
+          status : "failed",
+          message: "ID錯誤"
+      }))
+      res.end()
+      return;
+
+      }
+
+      const result = await AppDataSource.getRepository('Skill').delete(skillId );
+
+      if(result.affected === 0 ){
+        res.writeHead(400, headers)
+        res.write(JSON.stringify({
+        status : "failed",
+        message: "ID錯誤"
+      }))
+      res.end()
+      return;
+
+      }
+
+      res.writeHead(200, headers)
+      res.write(JSON.stringify({
+        status: "success",
+      }))
+      res.end()
+
+    } catch (error) {
+      errHandle(res);
+
+    }
+
+
+  }
+  else if (req.method === "OPTIONS") {
     res.writeHead(200, headers)
     res.end()
   } else {
